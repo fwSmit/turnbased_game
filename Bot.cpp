29d5b19e5ce1169cd6ba14da9fde08befde09779
fwsmit <fw.smit01@gmail.com>
2018-11-08 18:03:20 +0100

    alpha beta pruning

--- Bot.cpp
+++ Bot.cpp
@@ -1,76 +1,86 @@
 #include "Bot.h"
 
 
-Score operator-(const Score& input){
-	Score _score = input;
-	if(_score.result == Result::win){
-		_score.result = Result::loss;
-		return _score;
-	}
-	if(_score.result == Result::loss){
-		_score.result = Result::win;
-		return _score;
-	}
-	if(_score.result == Result::t_score){
-		_score.score = -_score.score;
-	}
-	return _score;
+//Score operator-(const Score& input){
+	//Score _score = input;
+	//if(_score.result == Result::win){
+		//_score.result = Result::loss;
+		//return _score;
+	//}
+	//if(_score.result == Result::loss){
+		//_score.result = Result::win;
+		//return _score;
+	//}
+	//if(_score.result == Result::t_score){
+		//_score.score = -_score.score;
+	//}
+	//return _score;
+//}
+
+//bool operator==(const Score& lhs, const Score& rhs){
+	//return lhs.result == rhs.result && lhs.score == rhs.score;
+//}
+bool operator==(const Score& lhs, const Score& rhs){
+	return lhs.score == rhs.score;
 }
 
-bool operator==(const Score& lhs, const Score& rhs){
-	return lhs.result == rhs.result && lhs.score == rhs.score;
+bool operator>(const Score& lhs, const Score& rhs){
+	return lhs.score > rhs.score;
 }
 
 // yet has to take depth into account
-bool operator>(const Score& lhs, const Score& rhs){
-	if (rhs.result == Result::empty_score) {
-		return true;
-	}
-	if (lhs.result == Result::empty_score) {
-		return false;
-	}
-	assert(lhs.result != Result::empty_score && rhs.result != Result::empty_score);
+//bool operator>(const Score& lhs, const Score& rhs){
+	//if (rhs.result == Result::empty_score) {
+		//return true;
+	//}
+	//if (lhs.result == Result::empty_score) {
+		//return false;
+	//}
+	//assert(lhs.result != Result::empty_score && rhs.result != Result::empty_score);
 
-	if(rhs.result == Result::win){
-		return false;
-	}
-	if(lhs.result == Result::win){ 
-		 return true;
-	 }
-	if(lhs.result == Result::loss) {return false;}
-	if(rhs.result == Result::loss) {return true;}
-	if(lhs.result == Result::t_score) {
-		 if(rhs.result == Result::t_score){
-			 return lhs.score > rhs.score;
-		 }
-		 // rhs must be draw
-		 if(lhs.score > 0){
-			 return true;
-		 }
-		 else{
-			 return false;
-		 }
-	}
-	if(lhs.result == Result::draw){
-		if(rhs.result == Result::t_score){
-			if(rhs.score < 0) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		if(rhs.result == Result::draw){
-			return false;
-		}
-	}
+	//if(rhs.result == Result::win){
+		//return false;
+	//}
+	//if(lhs.result == Result::win){ 
+		 //return true;
+	 //}
+	//if(lhs.result == Result::loss) {return false;}
+	//if(rhs.result == Result::loss) {return true;}
+	//if(lhs.result == Result::t_score) {
+		 //if(rhs.result == Result::t_score){
+			 //return lhs.score > rhs.score;
+		 //}
+		 //// rhs must be draw
+		 //if(lhs.score > 0){
+			 //return true;
+		 //}
+		 //else{
+			 //return false;
+		 //}
+	//}
+	//if(lhs.result == Result::draw){
+		//if(rhs.result == Result::t_score){
+			//if(rhs.score < 0) {
+				//return true;
+			//}
+			//else {
+				//return false;
+			//}
+		//}
+		//if(rhs.result == Result::draw){
+			//return false;
+		//}
+	//}
 
-	assert(0); // shouldnt reach this point
-	return false;
-}
+	//assert(0); // shouldnt reach this point
+	//return false;
+//}
 
 bool operator<(const Score& lhs, const Score& rhs){
 	return rhs > lhs;
+}
+bool operator>=(const Score& lhs, const Score& rhs){
+	return !(lhs > rhs);
 }
 
 void printScore(Score score){
--- Bot.h
+++ Bot.h
@@ -9,9 +9,10 @@ enum Result{win, loss, draw, empty_score, t_score /*some score is calculated*/};
 
 struct Score{
 	Result result;
-	int score;
+	float score = 0;
 	unsigned int depth;
-	Score(Result _result, unsigned int depth, int _score = 0) : result(_result), score(_score){}
+	Score(Result _result, unsigned int depth, float _score = 0) : result(_result), score(_score){}
+	Score(float _score) : score(_score){}
 };
 
 Score operator-(const Score& input);
@@ -23,11 +24,16 @@ bool operator>(const Score& lhs, const Score& rhs);
 
 bool operator<(const Score& lhs, const Score& rhs);
 
+bool operator>=(const Score& lhs, const Score& rhs);
+
 void printScore(Score score);
 
 template <class T>
 class Bot{
 	int number_evaluations = 0;
+	Score negativeInf;
+	Score positiveInf;
+	float aLot = 10000.f;
 	// depth starts at 0 and ends when it's equal to maxDepth (0 <= depth <= maxDepth)
 	// maxDepth is the maximum number of nodes the algorithm may travel down. If maxDepth is zero the algoritm will immediately return a value
 	Score getScore(T game, Move move, Score alpha, Score beta, unsigned int depth, const unsigned int maxDepth){
@@ -38,10 +44,12 @@ class Bot{
 			number_evaluations++;
 			if(isMaximizingPlayer){
 				//std::cout << "winning move of maximizing player" << std::endl;
-				return Score(Result::win, depth);
+				//return Score(Result::win, depth);
+				return Score(aLot);
 			}
 			else{
-				return Score(Result::loss, depth);
+				//return Score(Result::loss, depth);
+				return Score(-aLot);
 			}
 			////std::cout << "one way of winning/losing" << std::endl;
 			//number_evaluations++;
@@ -50,23 +58,26 @@ class Bot{
 
 		if(game.isBoardFull()){ // && !game.hasWon()
 			number_evaluations++;
-			return Score(Result::draw, depth);
+			//return Score(Result::draw, depth);
+			return Score(0);
 		}
 
 		if(depth == maxDepth) {
 			number_evaluations++;
-			return game.getBoardScore(depth);
+			//return game.getBoardScore(depth);
+			return Score(0);
 		}
 		if (isMaximizingPlayer) {
 			auto possibleMoves = game.getAvailableMoves();
-			Score value = Score(Result::empty_score, depth);
+			//Score value = Score(Result::empty_score, depth);
+			Score value = negativeInf;
 			//std::cout << "possible moves in getScore: " << possibleMoves.size() << std::endl;
 			game.nextPlayer();
 			for(int i = 0; i < possibleMoves.size(); i++){
 				Score score = getScore(game, possibleMoves[i], alpha, beta, depth+1, maxDepth);
 				value = std::max(score, value);
 				alpha = std::max(alpha, score);
-				if(alpha > beta){
+				if(alpha >= beta){
 					std::cout << "Breaking" << std::endl;
 					break;
 				}
@@ -76,14 +87,15 @@ class Bot{
 		}
 		else {
 			auto possibleMoves = game.getAvailableMoves();
-			Score value = Score(Result::empty_score, depth);
+			//Score value = Score(Result::win, depth);
+			Score value = positiveInf;
 			//std::cout << "possible moves in getScore: " << possibleMoves.size() << std::endl;
 			game.nextPlayer();
 			for(int i = 0; i < possibleMoves.size(); i++){
 				Score score = getScore(game, possibleMoves[i], alpha, beta, depth+1, maxDepth);
 				value = std::min(score, value);
 				beta = std::min(beta, score);
-				if(alpha > beta){
+				if(alpha >= beta){
 					std::cout << "Breaking" << std::endl;
 					break;
 				}
@@ -93,8 +105,71 @@ class Bot{
 		}
 		
 	}
+	//Score getScore(T game, Move move, Score alpha, Score beta, unsigned int depth, const unsigned int maxDepth){
+		//bool isMaximizingPlayer = (depth % 2) == 0;
+		////std::cout << "depth is " << depth << std::endl;
+		//game.playMove(move);
+		//if(game.hasWon()){
+			//number_evaluations++;
+			//if(isMaximizingPlayer){
+				////std::cout << "winning move of maximizing player" << std::endl;
+				//return Score(Result::win, depth);
+			//}
+			//else{
+				//return Score(Result::loss, depth);
+			//}
+			//////std::cout << "one way of winning/losing" << std::endl;
+			////number_evaluations++;
+			////return Score(Result::win, depth);
+		//}
+
+		//if(game.isBoardFull()){ // && !game.hasWon()
+			//number_evaluations++;
+			//return Score(Result::draw, depth);
+		//}
+
+		//if(depth == maxDepth) {
+			//number_evaluations++;
+			//return game.getBoardScore(depth);
+		//}
+		//if (isMaximizingPlayer) {
+			//auto possibleMoves = game.getAvailableMoves();
+			//Score value = Score(Result::empty_score, depth);
+			////std::cout << "possible moves in getScore: " << possibleMoves.size() << std::endl;
+			//game.nextPlayer();
+			//for(int i = 0; i < possibleMoves.size(); i++){
+				//Score score = getScore(game, possibleMoves[i], alpha, beta, depth+1, maxDepth);
+				//value = std::max(score, value);
+				//alpha = std::max(alpha, score);
+				//if(alpha > beta){
+					//std::cout << "Breaking" << std::endl;
+					//break;
+				//}
+			//}
+			//number_evaluations++;
+			//return value;
+		//}
+		//else {
+			//auto possibleMoves = game.getAvailableMoves();
+			////Score value = Score(Result::win, depth);
+			////std::cout << "possible moves in getScore: " << possibleMoves.size() << std::endl;
+			//game.nextPlayer();
+			//for(int i = 0; i < possibleMoves.size(); i++){
+				//Score score = getScore(game, possibleMoves[i], alpha, beta, depth+1, maxDepth);
+				//value = std::min(score, value);
+				//beta = std::min(beta, score);
+				//if(alpha > beta){
+					//std::cout << "Breaking" << std::endl;
+					//break;
+				//}
+			//}
+			//number_evaluations++;
+			//return value;
+		//}
+		
+	//}
 public:
-	Bot(){
+	Bot() : negativeInf(-std::numeric_limits<float>::infinity()), positiveInf(std::numeric_limits<float>::infinity()) {
 		std::srand(time(nullptr)); //set the seed to the current time
 	}
 
@@ -105,10 +180,12 @@ public:
 			std::cout << "no possble moves provided" << std::endl;
 		}
 		std::cout << "Possible move size " << possibleMoves.size() << std::endl;
-		Score bestScore = Score(Result::loss, 0);
+		//Score bestScore = Score(Result::loss, 0);
+		Score bestScore = negativeInf;
 		std::vector<Move> bestMoves;
 		for(int i = 0; i < possibleMoves.size(); i++){
-			Score score = getScore(game, possibleMoves[i], Score(Result::empty_score, 0) /*alpha*/, Score(Result::win, 0) /*beta*/, 0, maxDepth);
+			Score score = getScore(game, possibleMoves[i], negativeInf /*alpha*/, positiveInf /*beta*/, 0, maxDepth);
+			//Score score = getScore(game, possibleMoves[i], Score(Result::empty_score, 0) /*alpha*/, Score(Result::win, 0) /*beta*/, 0, maxDepth);
 			std::cout << "Move : " << possibleMoves[i].x+1 << "	";
 			printScore(score);
 			if(score.result == Result::win){
